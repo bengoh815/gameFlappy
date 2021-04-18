5e18ab2f23fa1c3ed3abce44e47f2a833da1cb24
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2021-04-18 23:15:32 +0800

    added infinite background feature

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ clock = pygame.time.Clock()
 
 # imports & initialization
 game_started = False
+score = 0
+object_move_speed = 2
+
 # background
+bg_x = 0
+bg1_x = 900
 bg = pygame.image.load("./static_images/bg.png")
-bg1 = pygame.image.load("./static_images/bg.png")
 
 # bird
 bird = pygame.image.load("./static_images/bird.png")
@@ -24,36 +28,58 @@ bird_jump_variable = 0
 # pipe
 pipe = pygame.image.load("./static_images/pipe.png")
 pipe_hitbox = pipe.get_rect(topleft=(650, 300))
+added_score = False
 
 # gravity
 gravity = 1
 
+# function
+# check background
+def bg_move(x):
+    if abs(x) >= 900 and x < 0:
+        return 900
+    return x
 
 
 while True:
-    # rendering
-    # bg
-    screen.blit(bg, (0, 0))
-    # bird
-    screen.blit(bird, bird_hitbox)
-    # pipe
-    screen.blit(pipe, pipe_hitbox)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                print('SPACEEEEEEEEE')
                 bird_jump_variable = bird_jump
 
     bird_hitbox.y -= bird_jump_variable
     bird_jump_variable -= gravity
 
-    print(bird_hitbox.y, bird_jump_variable)
+    # objects moving left
+    bg_x -= object_move_speed
+    bg1_x -= object_move_speed
+    pipe_hitbox.x -= object_move_speed
 
-    if bird_hitbox.y > 480:
-            sys.exit()
+    # rendering
+    # bg
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg1_x, 0))
+    # bird
+    screen.blit(bird, bird_hitbox)
+    # pipe
+    screen.blit(pipe, pipe_hitbox)
+
+    # check background
+    bg_x = bg_move(bg_x)
+    bg1_x = bg_move(bg1_x)
+
+    # check death
+    if bird_hitbox.colliderect(pipe_hitbox) or (bird_hitbox.y >= 480):
+        print('Game over~')
+        sys.exit()
 
     pygame.display.update()
     clock.tick(20)
+
+    # check points
+    if ((bird_hitbox.x + bird_hitbox.width) > (pipe_hitbox.x  + (pipe_hitbox.width / 2))) and added_score == False:
+        added_score = True
+        score += 1
+        print(score)
